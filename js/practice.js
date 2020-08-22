c434a61a79648aeb6012cc3ae508af054e16b6f0
michaelsattefield <michael.f.satterfield@gmail.com>
2020-08-21 21:35:51 -0500

    added javascript to yaddle page

---
+++ js/practice.js
@@ -0,0 +1,10 @@
+function yaddle(){
+    var firstQuestion = confirm("Do You Know About Yaddle?")
+    if(firstQuestion === true) {
+        alert("click on Yaddle's Pic for more info on the Jedi")
+    } else {
+        alert("Click on the Picture of the Yoda Looking Character for the Story of Yaddle! ")
+    }
+
+}
+yaddle()
